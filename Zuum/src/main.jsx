70436253428fb46089f7a7fb6b1f2e80c0b03d59
AleMel27 <andremarcasmelgar@gmail.com>
2026-03-10 18:48:02 -0500

    banner en proceso

--- Zuum/src/main.jsx
+++ Zuum/src/main.jsx
@@ -1,10 +1,14 @@
+import '../tailwind.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import Header from './pages/components/header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
+
   </StrictMode>,
 )
---
+++ Zuum/src/pages/components/header.jsx
@@ -0,0 +1,49 @@
+function Header() {
+  return (
+    <header className="flex justify-between items-center px-4 py-3 bg-pink-300 text-gray-700 text-sm">
+
+      {/* Soporte */}
+        <div>
+          <span className="block">Atención al cliente</span><br/>
+          <span className="font-semibold">123-456-789</span>
+        </div>
+
+      {/* Logo */}
+      <a href="index.html">
+        <img src="/logo.png" alt="logo" className="w-44" />
+      </a>
+
+      {/* Usuario y carrito */}
+      <div className="flex items-center gap-4">
+        {/* Carrito */}
+        <a href="canshop.html" className="text-2xl text-gray-700">
+          <i className="fa-solid fa-bag-shopping"></i>
+        </a>
+
+        {/* Usuario con dropdown */}
+        <div className="dropdown">
+          <button
+            className="btn btn-light dropdown-toggle flex items-center gap-2"
+            type="button"
+            data-bs-toggle="dropdown"
+          >
+            <i className="fa-solid fa-user"></i>
+            <span id="aUser">Iniciar Sesión</span>
+          </button>
+          <div className="dropdown-menu dropdown-menu-end p-2">
+            <div className="card border-0">
+              <div className="card-body p-2">
+                <a className="dropdown-item" href="perfil.html">Mi perfil</a>
+                <a className="dropdown-item" href="pedidos.html">Mis pedidos</a>
+                <a className="dropdown-item text-danger" href="logout.html">Cerrar sesión</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </header>
+  );
+}
+
+export default Header;
